acb91b65b61951d246ba47874406390cf5925cd9
Syndyr <connorday13@live.co.uk>
2018-11-30 22:42:38 +0100

    Image base class

---
+++ engine/classes/bases/image.lua
@@ -0,0 +1,13 @@
+local image = {}
+
+image.filename = "BASE"
+image.name = "BASE"
+image.type = "image"
+image.tile = false
+image.alpha = false
+image.trims = "standard"
+image.base = "e_base"
+image.author = "Monty"
+image.source = ""
+
+return image
