1a2c210e477d65854d0e0f74eb40bd8f46a37c45
Syndyr <connorday13@live.co.uk>
2018-12-02 01:12:34 +0100

    Changing asset library

--- libs/assets.lua
+++ libs/assets.lua
@@ -1,26 +1,12 @@
-assets = {}
+e.assets = {}
 
-function assets.load(folder)
-    assets[folder] = require("assets/"..folder)
+function e.assets.load(folder)
+    --e.assets[folder] = require("assets/"..folder)
+    --local ass = setmetatable(require("assets/"..folder), class.__superClass)
+    --local classy = class.getClass()
+    
 end
 
-assets = setmetatable(assets, {
-    __newindex = function(self, k, v)
-        if v.type == "image" then
-            local filepath = "assets/"..k..".png"
-
-            if v.filename ~= nil then filepath = "assets/"..k.."/"..v.filename..".png" end
-            v.image = love.graphics.newImage(filepath)
-        elseif v.type == "audio" then
-            
-        elseif v.type == "map" then
-                
-        elseif v.type == "save" then
-                
-        end
-        rawset(self, k, v)
-    end
-})
 --[[
 assets.load("tiles/forestFloor")
 assets.load("tiles/grass")
