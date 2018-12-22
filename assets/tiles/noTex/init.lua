0faa0e8c65f01f284af52dae4397b2ac448ca714
TNS Monty <connorday13@live.co.uk>
2018-12-22 16:30:21 +0000

    Added image header

---
+++ assets/tiles/noTex/init.lua
@@ -0,0 +1,11 @@
+local image = {}
+
+image.filename = "noTex"
+image.name = "noTex"
+image.type = "image"
+image.tile = true
+image.alpha = false
+image.trims = "standard"
+image.base = "e_image"
+
+return image
