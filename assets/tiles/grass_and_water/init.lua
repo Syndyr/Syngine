4bc7b3794c0fea2a7128ee36fe2aeca5432f3fd9
TNS Monty <Syndyr@users.noreply.github.com>
2023-11-11 22:06:02 +0000

    Update init.lua

--- assets/tiles/grass_and_water/init.lua
+++ assets/tiles/grass_and_water/init.lua
@@ -1,11 +1,16 @@
 return {
 
+    name = "grassAndWaterSheet",
+    type = "isoTileSet",
+    title = "",
+    filename = "grass_and_water_modified",
+    alpha = true,
     author = "Clint Bellanger",
     source = "https://opengameart.org/content/grass-and-water-tiles",
     version = "",
     description = "isometric tile set meant for early development only, do not ship.",
     
-    tileDictionary = require("dictionary")
+    tileDictionary = "dictionary"
 
 
 }
