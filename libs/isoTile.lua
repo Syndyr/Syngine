b937beb74486c21c989f7c67c0b8118ef6d17cbf
TNS Monty <Syndyr@users.noreply.github.com>
2023-11-11 02:04:00 +0000

    Update isoTile.lua
    Some dirty frustum culling testing

--- libs/isoTile.lua
+++ libs/isoTile.lua
@@ -82,7 +82,8 @@ end
 function t:getWorldCoordinate(screenCoordinate)
     
     --Invert matrix
-    
+    local screenCoordinate = e.vp - screenCoordinate
+
     local iHat = self.iHat
     local jHat = self.jHat
     local size = self.size
@@ -97,12 +98,18 @@ function t:getWorldCoordinate(screenCoordinate)
     
     
     return v( 
-        screenCoordinate.x * inver.a + screenCoordinate.y * inver.b, 
-        screenCoordinate.x * inver.c + screenCoordinate.y * inver.d
+        (((screenCoordinate.x * inver.a +  screenCoordinate.y * inver.b) + 0.5 ) * -1)*64, 
+        (((screenCoordinate.x * inver.c +  screenCoordinate.y * inver.d) - 0.5) * -1)*64
     )
 end
 
-t.testTiles = t:createTilePositions(32,32)
+function t:getTileCoordinateFromScreenPosition(pos)
+
+    return self:getWorldCoordinate(pos) / 64
+
+end
+
+t.testTiles = t:createTilePositions(512,512)
 
 function t:testingDraw()
     
@@ -111,16 +118,44 @@ function t:testingDraw()
     local tile = self.tileMesh
 
     local mpos = love.mouse.getPosition()
-            --:inBounds2D(b, p)
-    local mposInWorldSpace = self:getWorldCoordinate(VPOffset-mpos)
+    local mposInWorldSpace = self:getWorldCoordinate( mpos)
             
-    local mx, my = math.floor(mposInWorldSpace.x + 1.5)*-1,math.floor(mposInWorldSpace.y + 0.5)*-1
-    
+    local mx, my = mposInWorldSpace.x, mposInWorldSpace.y
+    local mosTile = self:getTileCoordinateFromScreenPosition( mpos )
+
     love.graphics.setColor(255,0,0,255)
-    --e.olgPrint(k..","..n, pos.x+VPOffset.x, pos.y+VPOffset.y)
-    e.olgPrint(mx..","..my, mpos.x, mpos.y-20)
+
+    local inCullAmount = 0
+    local bottomRight = self:getTileCoordinateFromScreenPosition(v(love.graphics.getDimensions() )) - inCullAmount
+    local bottomLeft = self:getTileCoordinateFromScreenPosition(v(love.graphics.getDimensions() ) * v(0,1)) - inCullAmount
+    local topRight = self:getTileCoordinateFromScreenPosition(v(love.graphics.getDimensions() ) * v(1,0)) + inCullAmount
+    local topLeft = self:getTileCoordinateFromScreenPosition(v()) + inCullAmount
+
+
+    e.olgPrint(mosTile.x..","..mosTile.y, mpos.x, mpos.y-20)
     
-    
+    local x = math.floor(math.max(0, topLeft.x))
+
+    local xLim = math.floor(math.min(#tiles, bottomRight.x))
+    e.oldprint(#tiles, bottomRight.x)
+    for x = x, xLim do 
+        local yLim = math.floor(math.min(#tiles[x], bottomLeft.y))
+        local y = math.floor(math.max(0, topRight.y))
+
+        for y=y, yLim do
+            
+            love.graphics.setColor(255,255,255,20)
+            
+            
+            if math.floor(mosTile.x) == x and math.floor(mosTile.y) == y then love.graphics.setColor(255,0,0,20) end 
+            
+            local pos = tiles[x][y] * self.size
+            e.olDraw(tile, pos.x+VPOffset.x, pos.y+VPOffset.y)
+
+        end
+
+    end 
+    --[[
     for k,v in pairs(tiles) do
         
         for n,b in pairs(v) do
@@ -128,18 +163,16 @@ function t:testingDraw()
             love.graphics.setColor(255,255,255,20)
             
             
-            if mx == k and my == n then love.graphics.setColor(255,0,0,20) end 
-            
+            if math.floor(mosTile.x) == k and math.floor(mosTile.y) == n then love.graphics.setColor(255,0,0,20) end 
             
             local pos = b * self.size
             e.olDraw(tile, pos.x+VPOffset.x, pos.y+VPOffset.y)
             
             
-            
-            
         end
         
     end
+    ]]--
     
 end
 
